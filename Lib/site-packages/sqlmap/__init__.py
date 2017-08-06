3e1544e4bce5f4eda6ae1adef37157a9d2c08efb
sui84 <sui84@126.com>
2017-08-06 22:43:44 +0800

    Check in for python27 update

---
+++ Lib/site-packages/sqlmap/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2017 sqlmap developers (http://sqlmap.org/)
+See the file 'doc/COPYING' for copying permission
+"""
+
+import os
+import sys
+
+sys.dont_write_bytecode = True
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
